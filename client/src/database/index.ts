a8bb52b67f29163d1a2451ff88f5262016b33a8f
fede-rodes <federodes@gmail.com>
2022-02-23 11:21:37 +0100

    format

--- client/src/database/index.ts
+++ client/src/database/index.ts
@@ -2,8 +2,8 @@ import fs from 'fs/promises'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 interface INFT {
   tokenId?: number
